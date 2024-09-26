4271f3610a78c273b029f63e03e600173b326b0f
mbutler808 <mbutler@hawaii.edu>
2024-09-26 12:36:13 -1000

    updated toad heart lab

---
+++ _posts-hidden/hadrosaur.R
@@ -0,0 +1,36 @@
+m = 2500*1000
+bmr = 50.4*(m)^.73/1000 # in kJ/hr
+rmr = 1.2*bmr in kJ/hr
+
+### Hc
+Tb = 37
+Ta=30
+deltaT = Tb-Ta
+
+C=20.5*m^-.426
+Hc= C*m*deltaT
+
+### He
+Xexp = 44 # mg/l air
+Xinsp = 30.4 * .75 # mg/l 
+VO2 = rmr*24 / (20*.2*.25)   # kJ/hr * 24 hr/day / 20
+
+REWL = VO2*(Xexp - Xinsp)
+
+l = 400  # cm lenth torso
+r = 87.5 # cm radius torso
+SAtorso = 2*pi*r*l + 2*pi*r^2 # 268017.1cm2
+
+l=600 # cm tail length
+r=25  # cm tail radius
+SAtail = 2*pi*r*l  # 94247.78 cm2
+
+SA = SAtorso + SAtail
+resistance = 100 # sec/cm
+CEWL = (Xexp - Xinsp)*SA/resistance/1000
+CEWL = CEWL/1000*60*60*24
+
+EWL = (REWL + CEWL)
+Qevap = 2.4
+He = EWL*Qevap
+He/24
